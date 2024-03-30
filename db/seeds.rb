a52a552a9a828b9c7cb3294f2c519b41a76d9d7b
brolinr <remunyangabrolin@gmail.com>
2024-03-30 12:43:46 +0200

    finished with the defaults

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,9 @@
-# DatabaseCleaner.clean_with(:truncation) if ENV.fetch('DB_CLEANER', 'OFF') == 'ON'
-#
-# seed_files = Dir[Rails.root.join('db/seeds/*.rb')].sort
-# puts 'Seeds empty!' if seed_files.blank?
-#
-# seed_files.each do |file|
-#   puts "Loading seed: #{file}"
-#   require file
-# end
+DatabaseCleaner.clean_with(:truncation) if ENV.fetch('DB_CLEANER', 'OFF') == 'ON'
 
-Administrator.create!(
-  email: 'admin@analytica.co.zw',
-  password: 'analytica263#',
-  password_confirmation: 'analytica263#'
-)
+seed_files = Dir[Rails.root.join('db/seeds/*.rb')].sort
+puts 'Seeds empty!' if seed_files.blank?
+
+seed_files.each do |file|
+  puts "Loading seed: #{file}"
+  require file
+end
